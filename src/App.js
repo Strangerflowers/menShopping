ccde85f2e05d458afb29de854f4f4f1c94a4eb41
zzq <3074127187@qq.com>
2018-12-03 16:27:39 +0800

    less

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 // import { Route} from "react-router-dom";
 
 import Index from './pages/Index.jsx';
+
 // import Detail from './pages/detail.jsx';
 // import './styles/App.css';
 
 class App extends Component {
+ 
   render() {
     return (
       <div className="App">
--- src/components/Wheader.jsx
+++ src/components/Wheader.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import style from '../styles/index.less';
+
 class Mheader extends React.Component{
   constructor(props){
     super(props);
     this.props=props;
   }
+  
   render(){
     return (
          <header id="header">
+          wenjian jia 
              <div className="logo"></div>
              <div className="header-wrap"> <a href="tmpl/search.html" className="header-inp"> <i className="icon"></i> <span className="search-input" id="keyword" placeholder=""></span> </a> </div>
              <div className="header-r"><a id="header-nav" href="tmpl/member/order_list.html"><i className="nsgdingdan"></i>
