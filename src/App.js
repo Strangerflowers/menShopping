1dce17e0dc6a5899ac989bc8baa7f312b852add6
zzq <3074127187@qq.com>
2018-12-03 14:09:36 +0800

    搭建脚手架

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route} from "react-router-dom";
+
+import Index from './pages/Index.jsx';
+import Detail from './pages/detail.jsx';
+// import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <Redirect exact from='/' to='/home/hot/'/> */}
+      		
+            <Route path="/home/" component={Index} />
+            <Route path="/detail/" component={Detail}/>
+          
       </div>
     );
   }
